16fbdd234d29e2258fe5d478fb52c8ccede31c86
Tyler Wong <tylerwong.j@gmail.com>
2020-05-19 00:15:22 -0700

    master - firebase working with project

--- public/script.js
+++ public/script.js
@@ -2,11 +2,11 @@ var city = "Los Angeles";
 
 function getCityTemperature() {
 	$.getJSON(
-		`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=78b1c75a5e39cee7c2e02f82d496f981`,
+		`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=78b1c75a5e39cee7c2e02f82d496f981`,
 		function(data) {
 			console.log(data);
 
-			var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+			var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
 			var temp = Math.round(data.main.temp);
 			var weather = data.weather[0].main;
 
