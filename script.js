625aa05688978cb1ee7c8fcc1a3bce5bb8dd01ab
Tyler Wong <tylerwong.j@gmail.com>
2020-05-18 23:42:41 -0700

    master - working script with API calls - fixed up UI as well

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-$.getJSON(
-	"http://api.openweathermap.org/data/2.5/weather?q=Los%20Angeles&units=imperial&APPID=78b1c75a5e39cee7c2e02f82d496f981",
-	function(data) {
-		console.log(data);
+var city = "Los Angeles";
 
-		var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-		var temp = Math.round(data.main.temp);
-		var weather = data.weather[0].main;
+function getCityTemperature() {
+	$.getJSON(
+		`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=78b1c75a5e39cee7c2e02f82d496f981`,
+		function(data) {
+			console.log(data);
 
-		$('.js-icon').attr('src', icon);
-		$('.js-weather').append(weather);
-		$('.js-temp').prepend(temp);
-	}
-)
+			var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+			var temp = Math.round(data.main.temp);
+			var weather = data.weather[0].main;
+
+			$('.js-city').text(city);
+			$('.js-icon').attr('src', icon);
+			$('.js-weather').text(weather);
+			$('.js-temp').text(temp);
+		}
+	)
+}
+
+$('.js-form').submit(function(e) {
+	e.preventDefault();
+
+	city = $('.js-input').val();
+
+	getCityTemperature();
+})
+
+getCityTemperature();
